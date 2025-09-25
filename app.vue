231b9503d1a21b4df90da02c18a87fa4bad695ee
Zhanna <kezhanna.03@mail.ru>
2025-09-26 01:49:37 +0500

    added tailwindcss

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
+import '/assets/css/tailwind.css'
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+      <NuxtPage/>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-black text-[orange] rounded-8">
+        Hi
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
