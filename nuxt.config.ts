981fd29cda9988a2e83425d041690d375a473ce6
Zhanna <kezhanna.03@mail.ru>
2025-09-26 02:40:43 +0500

    added header with bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image'
+  ],
+
   css: ['./assets/css/tailwind.css'],
+
+  image: {
+    screens: { xs: 320, sm: 640, md: 768, lg: 1024, xl: 1280 }
+  },
+
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
---
+++ pages/wine.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="max-w-[1420px] w-full mx-auto flex justify-between pt-2">
+    <div class="max-w-[285px] w-full flex justify-between">
+      <div class="flex">
+        <NuxtImg></NuxtImg>
+        <p>Алматы</p>
+      </div>
+      <div class="flex">
+        <NuxtImg></NuxtImg>
+        <p>Адреса винотек</p>
+      </div>
+    </div>
+    <div class="max-w-[392px] w-full flex justify-between">
+      <p>Для бизнеса</p>
+      <p>Оплата и доставка</p>
+      <p>Гарантия</p>
+    </div>
+  </div>
+  <div class="max-w-[1420px] w-full mx-auto flex justify-between pt-2">
+    <div>
+      <NuxtImg src="/images/logo.svg" alt="Logo" width="168" height="36"></NuxtImg>
+    </div>
+    <div>
+      <NuxtImg></NuxtImg>
+      <p></p>
+    </div>
+    <div>
+      <input type="search" />
+      <NuxtImg></NuxtImg>
+    </div>
+    <div>
+      <NuxtImg></NuxtImg>
+      <NuxtImg></NuxtImg>
+    </div>
+  </div>
+  <div class="max-w-[1420px] w-full mx-auto bg-[#efe3d1] rounded-[6px] p-[10px]">
+    <div class=" text-center text-[14px]  text-[#1A1A1A] mr-[10px] ml-[16px]">
+        Обновленный сайт запущен в бета-версии. В редких случаях возможны неполадки. Благодарим за поддержку!
+    </div>
+  </div>
+  <div class="max-w-[1420px] w-full mx-auto flex justify-between mt-4 text-[16px]">
+    <div class="bg-[#f5f5f5] text-[#1a1a1a] py-2 px-4 rounded-[6px]">Вино</div>
+    <div class=" text-[#1a1a1a] py-2 px-4 rounded-[6px]">Игристые вина</div>
+    <div class=" text-[#1a1a1a] py-2 px-4 rounded-[6px]">Крепкий алкоголь</div>
+    <div class=" text-[#1a1a1a] py-2 px-4 rounded-[6px]">Ликеры и Биттеры </div>
+    <div class=" text-[#1a1a1a] py-2 px-4 rounded-[6px]">Сеты</div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
