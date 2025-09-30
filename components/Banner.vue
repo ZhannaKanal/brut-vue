7e4dc4875ddfbbab38b0c85d63106f6b7856d32f
Zhanna <kezhanna.03@mail.ru>
2025-09-30 23:46:46 +0500

    added banenr

---
+++ components/Banner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper-banner px-[80px] w-full mx-auto pt-[24px] pb-[40px]">
+    <div class="banner bg-[#000] p-[40px] rounded-[24px] min-h-[207px] ">
+      <div class="banner-title text-[#fff] text-[32px] mb-[24px] leading-[100%]">Вино</div>
+      <p class="banner-description text-[#fff] text-[20px] leading-[120%]">Классические и аутентичные, популярные и известные лишь немногим знатокам, молодые и выдержанные, для особых случаев, подарков коллегам и встреч с друзьями! Наши эксперты выбрали лучшее, что представлено в мире: перед вами впечатляющая коллекция вин BRUT!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/wine.vue
+++ pages/wine.vue
@@ -2,12 +2,14 @@
   <div>
     <BaseHeader />
     <BreadCrumbs />
+    <Banner />
   </div>
 </template>
 
 <script setup>
 import BaseHeader from '~/components/BaseHeader.vue'
 import BreadCrumbs from '~/components/BreadCrumbs.vue'
+import Banner from '~/components/Banner.vue';
 </script>
 
 <style lang="scss" scoped>
