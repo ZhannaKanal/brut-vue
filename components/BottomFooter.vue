c290202ced69fa01b9a6ffc90d5c154b2db18132
Zhanna <kezhanna.03@mail.ru>
2025-10-01 23:54:07 +0500

    added content of bottomfooter

--- components/BottomFooter.vue
+++ components/BottomFooter.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="px-[80px] w-full mx-auto bg-[#1c100d] h-[100px]">
-
+        <div class="text-[#fff] py-[32px] flex justify-start gap-[60px]">
+            <p>© BRUT, 2025</p>
+            <a href=""> Пользовательское соглашение </a>
+            <a href=""> Политика конфиденциальности </a>
+            <a href=""> Карта сайта</a>
+        </div>
     </div>
 </template>
 
